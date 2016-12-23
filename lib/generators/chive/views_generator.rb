61986c0adfe422957f5d3ba6e47db6bf687e18d7
Fred Snyder <fsnyder@castwide.com>
2016-12-22 21:05:51 -0500

    Views generator uses directory method and supports destroy.

--- lib/generators/chive/views_generator.rb
+++ lib/generators/chive/views_generator.rb
@@ -1,7 +1,8 @@
 module Chive
   class ViewsGenerator < Rails::Generators::Base
+    source_root Chive::Engine.root
     def generate_views
-      FileUtils.cp_r File.join(Chive::Engine.root, 'app', 'views', 'chive'), File.join(Rails.root, 'app', 'views', 'chive')
+      directory File.join('app', 'views', 'chive'), File.join(Rails.root, 'app', 'views', 'chive')
     end
   end
 end
