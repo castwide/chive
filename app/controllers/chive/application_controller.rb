07c3d0ef3639457ece41f47778e29c771b735274
Fred Snyder <fsnyder@castwide.com>
2016-12-25 07:50:45 -0500

    Changed ArticlesHelper initialization.

--- app/controllers/chive/application_controller.rb
+++ app/controllers/chive/application_controller.rb
@@ -1,6 +1,8 @@
 module Chive
   class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
-    helper :all
+    config.to_prepare do
+      ApplicationController.helper Chive::ArticlesHelper
+    end
   end
 end
